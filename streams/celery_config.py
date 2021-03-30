a8187e999fe59ffff7ebbe8844f6b1d3876b85ea
LilSpazJoekp <15524072+LilSpazJoekp@users.noreply.github.com>
2021-03-30 18:18:39 -0500

    change backend to managed cache

--- streams/celery_config.py
+++ streams/celery_config.py
@@ -1,6 +1,6 @@
 # from . import services
 
-result_backend = "redis://localhost/2"
+result_backend = "redis//default:ks1e5cgos3pfxrtj@db-redis-nyc1-97053-do-user-4054415-0.b.db.ondigitalocean.com:25061"
 include = ["streams.tasks"]
 accept_content = ["pickle"]
 result_serializer = "pickle"
